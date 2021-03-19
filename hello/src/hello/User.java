99e6c334fc1221ca0b641710343c74900622dc16
Svetlio <Svetlio@DESKTOP-HOU6L3O>
2021-03-19 17:13:43 +0200

    commit

---
+++ hello/src/hello/User.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class User {
+	
+	private String yourName;
+
+	public String getMyName() {
+		return yourName;
+	}
+
+	public void setMyName(String myName) {
+		this.yourName = myName;
+	}
+	
+	
+
+}
--- hello/src/hello/project.java
+++ hello/src/hello/project.java
@@ -2,7 +2,8 @@ package hello;
 
 public class project {
 	public static void main(String[] args) {
-		System.out.println("Helloñ, World!");
+		System.out.println("Hello, World!");
+		System.out.println("Hello My World!");
 
 	}
 }
